fb728a1923cd9a4b87f2253e0967723bddfe1153
3108pranav <127076901+3108pranav@users.noreply.github.com>
2023-03-10 22:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,86 +1,126 @@
-// let userform=document.getElementsById("userform");
-// const saveuserform=(event)=>{
-//     event.preventDefault();
-//     const name=document.getElementsById("name").value;
-//     const email=document.getElementsById("email").value;
-//     const Password=document.getElementsById("password").value;
-// }
-// userform.addEventListner("submit",saveuserform)
+let element = (id) => document.getElementById(id);
 
+let classes = (classes) => document.getElementsByClassName(classes);
 
+let user_entries = [];
 
-
-
-// const dobInput = document.getElementById('dob');
-// const dobError = document.getElementById('dob-error');
-
-// dobInput.addEventListener('input', () => {
-//   const dobValue = dobInput.value;
-//   const dobDate = new Date(dobValue);
-//   const age = calculateAge(dobDate);
-  
-//   if (age < 18) {
-//     dobError.textContent = 'You must be at least 18 years old.';
-//     dobInput.setCustomValidity('You must be at least 18 years old.');
-//   } else if (age > 55) {
-//     dobError.textContent = 'You must be no more than 55 years old.';
-//     dobInput.setCustomValidity('You must be no more than 55 years old.');
-//   } else {
-//     dobError.textContent = '';
-//     dobInput.setCustomValidity('');
-//   }
-// });
-
-// function calculateAge(dob) {
-//   const today = new Date();
-//   const diff = today.getTime() - dob.getTime();
-//   const ageDate = new Date(diff);
-//   return Math.abs(ageDate.getUTCFullYear() - 1970);
-// }
-
-
-// THIS FUNCTION IS FOR DISPLAY DATA............................
-
-function store(){
-    
-    let inputEmail= document.getElementById("email");
-    let nameEl = document.getElementById("name");
-    let pass = document.getElementById("pass");
-    let dob = document.getElementById("dob");
-    localStorage.setItem("name", inputEmail.value);
-    localStorage.setItem("email", nameEl.value);
-    localStorage.setItem("pass", pass.value);
-    localStorage.setItem("dob", dob.value);
-    let n = localStorage.getItem("name");
-    let e = localStorage.getItem("email");
-    let p = localStorage.getItem("pass");
-    let dat = localStorage.getItem("dob");
-    var d = new Date(dob.value);
-    console.log(d);
-    let age = Math.floor((new Date() - d) / (365 * 24 * 60 * 60 * 1000));
-    console.log(age);
-    if(age<=18 || age>=55){
-        alert("Your Age is NOT satisfied by our CONDITIONS")
-        return
+function fillTable(){
+    let obj = localStorage.getItem("user_entries");
+    if(obj){
+        user_entries = JSON.parse(obj);
+    }else{
+        user_entries = [];
     }
-    document.querySelector(".local-entries").insertAdjacentHTML('beforeend',`<h3 class="boxed">${n}</h3>
-    <h3 class="boxed">${e}</h3>
-    <h3 class="boxed">${p}</h3>
-    <h3 class="boxed">${dat}</h3>`)
-    
-    // console.log(calculateAge(dat));
-    // function displayData() {
-    //     let output = document.getElementById('output');
-    //     output.insertAdjacentHTML("beforeend","<p>Name: " + n + "</p>" +
-    //                         "<p>Email: " + e + "</p>"+
-    //                         "<p>password: " + p + "</p>"+
-    //                         "<p>date: " + dat + "</p>")
-    //     }
-        
-    // displayData();
+    return user_entries;
+}
+user_entries = fillTable();
+
+let username = element("name"),
+  email = element("email"),
+  password = element("password"),
+  tc = element("tc"),
+  dob = element("dob");
+
+let errormsg = classes("errormsg");
+
+let form = element("form");
+
+function verify(elem,message,cnd){
+    if(cnd){
+        elem.style.border = "2px solid red";
+        elem.setCustomValidity(message);
+        elem.reportValidity();
+    }else{
+        elem.style.border = "2px solid green";
+        elem.setCustomValidity('');
+
+    }
+}
+
+function checkDOB(){
+    let age = new Date().getFullYear() - new Date(dob.value).getFullYear();
+    if(age < 18 || age>55){
+        return false;
+    }else{
+        return true;
+    }
+}
+let message_name = "Username must be at least 3 characters long";
+let message_email = "Email must be valid";
+let message_agree = "You must agree to the terms and conditions";
+let message_dob = "You age must be between 18 and 55 to continue";
+
+username.addEventListener("input", (e) => {
+    let cond_name = username.value.length < 3;
+    e.preventDefault();
+    verify(username,message_name,cond_name);
+});
+
+email.addEventListener("input", (e) => {
+    let cond_email = !(email.value.includes("@") && email.value.includes("."));
+    e.preventDefault();
+    verify(email,message_email,cond_email);
+});
+
+dob.addEventListener("input", (e) => {
+    let cond_dob = !checkDOB();
+    e.preventDefault();
+    verify(dob,message_dob,cond_dob);
+});
+tc.addEventListener("input", (e) => {
+    let cond_agree = !tc.checked;
+    e.preventDefault();
+    verify(tc,message_agree,cond_agree);
+});
+
+function makeObject(){
+    let check = false;
+    if(tc.checked){
+        check = true;
+    }
+    let obj = {
+        name: username.value,
+        email: email.value,
+        password: password.value,
+        dob: dob.value,
+        checked: check
+    }
+    return obj;
 }
 
 
+function displayTable(){
+    let table = element("user-table");
+    let entries = user_entries;
+    let str = `<tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Dob</th>
+                    <th>Accepted terms?</th>
+                </tr>\n`;
+    for(let i=0;i<entries.length;i++){
+        str += `<tr>
+                    <td>${entries[i].name}</td>
+                    <td>${entries[i].email}</td>
+                    <td>${entries[i].password}</td>
+                    <td>${entries[i].dob}</td>
+                    <td>${entries[i].checked}</td>
+                </tr>\n`;
+    }
+    table.innerHTML = str;
+}
 
-
-
+form.addEventListener("submit", (e) => {
+    let cond_agree= !tc.checked;
+    e.preventDefault();
+    if (!cond_agree) {
+        let obj = makeObject();
+        user_entries.push(obj);
+        localStorage.setItem("user_entries", JSON.stringify(user_entries));
+    }
+    displayTable();
+});
+window.onload = (event) => {
+    displayTable();
+};
